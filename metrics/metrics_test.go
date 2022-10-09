7a82e211fd0bc0512427a4dd18115d372a411746
João Fernandes <joaofnds@joaofnds.com>
2022-10-09 18:13:42 -0300

    test: add metrics server test

---
+++ metrics/metrics_test.go
@@ -0,0 +1,50 @@
+package metrics_test
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"testing"
+	"web/config"
+	"web/metrics"
+	"web/test"
+	. "web/test/matchers"
+
+	"go.uber.org/fx"
+	"go.uber.org/fx/fxtest"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+func TestHealth(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "/health suite")
+}
+
+var _ = Describe("/", Ordered, func() {
+	var url string
+
+	BeforeAll(func() {
+		var cfg config.AppConfig
+		fxtest.New(
+			GinkgoT(),
+			test.NopLogger,
+			config.Module,
+			metrics.Module,
+			fx.Populate(&cfg),
+		).RequireStart()
+		url = fmt.Sprintf("http://%s/metrics", cfg.MetricsAddr)
+	})
+
+	It("returns status OK", func() {
+		res := Must2(http.Get(url))
+		Expect(res.StatusCode).To(Equal(http.StatusOK))
+	})
+
+	It("returns how many requests were made", func() {
+		res := Must2(http.Get(url))
+		b := Must2(io.ReadAll(res.Body))
+		Expect(b).To(ContainSubstring(`promhttp_metric_handler_requests_total{code="200"} 1`))
+	})
+})
