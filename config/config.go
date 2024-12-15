3c9b0b3a8b6cf032d297c9deccf7add413c9dac1
João Fernandes <joaofnds@joaofnds.com>
2024-12-15 17:44:09 -0300

    refactor: sort config keys

--- config/config.go
+++ config/config.go
@@ -21,26 +21,26 @@ var (
 		"config",
 
 		fx.Provide(NewAppConfig),
-		fx.Provide(func(config AppConfig) logger.Config { return config.Logger }),
-		fx.Provide(func(config AppConfig) http.Config { return config.HTTP }),
-		fx.Provide(func(config AppConfig) metrics.Config { return config.Metrics }),
-		fx.Provide(func(config AppConfig) postgres.Config { return config.Postgres }),
-		fx.Provide(func(config AppConfig) mongo.Config { return config.Mongo }),
-		fx.Provide(func(config AppConfig) redis.Config { return config.Redis }),
 		fx.Provide(func(config AppConfig) featureflags.Config { return config.FeatureFlags }),
+		fx.Provide(func(config AppConfig) http.Config { return config.HTTP }),
+		fx.Provide(func(config AppConfig) logger.Config { return config.Logger }),
+		fx.Provide(func(config AppConfig) metrics.Config { return config.Metrics }),
+		fx.Provide(func(config AppConfig) mongo.Config { return config.Mongo }),
+		fx.Provide(func(config AppConfig) postgres.Config { return config.Postgres }),
+		fx.Provide(func(config AppConfig) redis.Config { return config.Redis }),
 		fx.Provide(func(config AppConfig) tracing.Config { return config.Tracing }),
 	)
 )
 
 type AppConfig struct {
 	Env          string              `mapstructure:"env" validate:"required,oneof=development staging production"`
-	Logger       logger.Config       `mapstructure:"logger" validate:"required"`
-	HTTP         http.Config         `mapstructure:"http" validate:"required"`
-	Metrics      metrics.Config      `mapstructure:"metrics" validate:"required"`
-	Postgres     postgres.Config     `mapstructure:"postgres" validate:"required"`
-	Mongo        mongo.Config        `mapstructure:"mongo" validate:"required"`
-	Redis        redis.Config        `mapstructure:"redis" validate:"required"`
 	FeatureFlags featureflags.Config `mapstructure:"feature_flags" validate:"required"`
+	HTTP         http.Config         `mapstructure:"http" validate:"required"`
+	Logger       logger.Config       `mapstructure:"logger" validate:"required"`
+	Metrics      metrics.Config      `mapstructure:"metrics" validate:"required"`
+	Mongo        mongo.Config        `mapstructure:"mongo" validate:"required"`
+	Postgres     postgres.Config     `mapstructure:"postgres" validate:"required"`
+	Redis        redis.Config        `mapstructure:"redis" validate:"required"`
 	Tracing      tracing.Config      `mapstructure:"tracing" validate:"required"`
 }
 
