4f54bb45f4419a8b6afcd8ee7804efaf06305a0e
João Fernandes <joaofnds@joaofnds.com>
2024-12-30 22:57:47 -0300

    chore: make email unique

--- cmd/migrate/migrations/20230521004811_create_users_table.sql
+++ cmd/migrate/migrations/20230521004811_create_users_table.sql
@@ -1,9 +1,9 @@
 -- +goose Up
-CREATE TABLE users(
-  id UUID PRIMARY KEY,
-  email VARCHAR NOT NULL,
-  created_at TIMESTAMP NOT NULL,
-  updated_at TIMESTAMP NOT NULL
+CREATE TABLE users (
+    id UUID PRIMARY KEY,
+    email VARCHAR NOT NULL UNIQUE,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL
 );
 
 -- +goose Down
